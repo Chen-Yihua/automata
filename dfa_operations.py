ac36896b2476e27c501449a612dd60be17874c55
Chen-Yihua <eva.chen900901@gmail.com>
2024-11-08 13:26:16 +0800

    改成用DFA提供的的畫圖方式

--- dfa_operations.py
+++ dfa_operations.py
@@ -1,15 +1,30 @@
 from graphviz import Digraph
 from automata.fa.dfa import DFA
 
-# 將邊由編號改成文字
-def get_key_from_value(mapping, value): 
-        for key, val in mapping.items():
-            if val == value:
-                return key
-        return None
+edge_mapping = {
+    'p != 0': '0',
+    'n >= 0': '1',
+    'p == 0': '2',
+    'n == 0': '3',
+    'n != 0': '4',
+    'p = 0': '5',
+    'n = n - 1': '6'
+}
 
-"""畫出 dfa"""
-def draw_dfa(dfa, edge_mapping):
+# 將邊由文字改成編號
+def get_key_from_value(mapping, value): 
+    for key, val in mapping.items():
+        if val == value:
+            return key
+    return None
+
+
+# 從 symbol，取得表達式
+def symbol_to_value(symbol):
+    return get_key_from_value(edge_mapping, symbol)
+
+# """畫出 dfa"""
+def draw_dfa(dfa):
     G = {}
     dot = Digraph()
     states = dfa.states
@@ -29,18 +44,36 @@ def draw_dfa(dfa, edge_mapping):
     dot.render('dfa', format='png', view=True)
     return G
 
+"""從兩點找邊 symbol"""
+def get_symbol(G, start , end):
+    for edge in G.edges():
+        if start == edge[0] and end == edge[1]:
+            return edge.attr["label"]
+
+"""從兩點找表達式"""
+def find_edge(G, start , end):
+    label = get_symbol(G, start , end)
+    value = symbol_to_value(label)
+    return value
+    
+
 """製作 DFA"""
-def build_dfa(G, path, mapping, start, end, unsat_condition):
+def build_dfa(G, path, mapping, initial_node, error_node, unsat_condition):
     states = set()
     input_symbols = set()
     unsat_node = []
 
-    # 製作 input_symbols
-    for edge, properties in G.items():
-        input_symbols.add(mapping[properties['label']])
-        if properties['label'] == unsat_condition and edge[0] != edge[1]: # 找出 unsat node
-            unsat_node.append(edge[0])
-        states.add(str(edge[0]))
+    # 製作 input_symbols、states
+    total_edges = G.edges()
+    for start, end in total_edges:
+        if initial_node != end:
+            symbol = get_symbol(G, start, end)
+            label = str(symbol_to_value(symbol))
+            if start != end and symbol != '':
+                input_symbols.add(symbol)
+            if label == unsat_condition and start != end: # 找出 unsat node
+                unsat_node.append(start)
+            states.add(str(start))
     # 添加 reject state
     if unsat_condition != 0:
         states.add('reject_node')
@@ -49,24 +82,27 @@ def build_dfa(G, path, mapping, start, end, unsat_condition):
                    for state in states}  # 初始化 transition
     # 對於 path 中每個點，找出與此點相連的邊，並調整其 transition，將點相連
     for i in range(len(path)-1):
-        for edge, properties in G.items():
-            if edge[0] == path[i] and edge[1] == path[i+1]:  
-                transitions[str(path[i])][mapping.get(properties['label'])] = str(edge[1]) 
+        for start, end in total_edges:
+            symbol = get_symbol(G, start, end)
+            if start == path[i] and end == path[i+1]:  
+                transitions[str(path[i])][symbol] = str(end) 
             
     # 將 unsat node 連至 reject_node
     for node in unsat_node:
         if node not in path:
             transitions[str(node)][mapping.get(unsat_condition)] = 'reject_node'
             # 若 unsat_node 不在 path 裡，更改走進 unsat node 的邊
-            for edge, properties in G.items():
-                if edge[1] == node:
-                    transitions[str(edge[0])][mapping.get(properties['label'])] = str(node)
+            for edge in G.edges():
+                if edge[0] != edge[1]:
+                    label = get_symbol(G, edge[0], edge[1])
+                    if edge[1] == node:
+                        transitions[str(edge[0])][label] = str(node)
 
     dfa = DFA(
         states = states,
         input_symbols = input_symbols,
         transitions = transitions,
-        initial_state = str(start),
-        final_states = {str(end)}
+        initial_state = str(initial_node),
+        final_states = {str(error_node)}
     )
     return dfa

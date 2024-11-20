bbe4d20d949af6ce825da97720e9693ad716ee08
Chen-Yihua <eva.chen900901@gmail.com>
2024-11-20 15:05:30 +0800

    增加update_dfa()

--- dfa_operations.py
+++ dfa_operations.py
@@ -3,8 +3,6 @@ from automata.fa.dfa import DFA
 from pysmt.typing import INT
 from pysmt.shortcuts import Symbol
 from z3 import *
-import unsat_core_operations
-import re
 
 edge_mapping = {
     'p != 0': '0',
@@ -83,14 +81,66 @@ def find_edge(G, start , end):
     label = get_symbol(G, start , end)
     value = symbol_to_value(label)
     return value
+
+def update_dfa(G, mapping, initial_node, error_node, unsat_condition, unsat_core):
+    states = set()
+    paths = []
+    input_symbols = set()
+    unsat_node = {}
+
+    # 添加 state
+    states.add(initial_node)
+    paths.append(initial_node)
+    for i in range(len(unsat_core)):
+        if i != len(unsat_core)-1:
+            states.add(f"added_node{i}")
+            paths.append(f"added_node{i}")
+            unsat_node[f"added_node{i}"] = unsat_core[i]
+    states.add(error_node)
+    paths.append(error_node)
+
+    if unsat_condition != 0: # 添加 reject state
+        states.add('reject_node')
+        paths.append('reject_node')
+
+    # 製作 input_symbols
+    total_edges = G.edges()
+    for start, end in total_edges:
+        if initial_node != end:
+            symbol = get_symbol(G, start, end)
+            if start != end and symbol != '':
+                input_symbols.add(symbol)
+
+    # 製作 transitions
+    transitions = {state: {value: state for key, value in mapping.items()} for state in paths}  # 初始化 transition
+    # 調整 transition，將點相連  
+    for node in unsat_node:
+        symbol = mapping[str(unsat_node[node])]
+        for i in range(len(paths)-1):
+            if paths[i+1] == node:
+                transitions[paths[i]][symbol] = node
+        transitions[node][mapping.get(unsat_condition)] = 'reject_node' 
+    last_key, _ = unsat_node.popitem()        
+    transitions[last_key][mapping[str(unsat_core[-1])]] = error_node
+
+                
+    dfa = DFA(
+        states = states,
+        input_symbols = input_symbols,
+        transitions = transitions,
+        initial_state = str(initial_node),
+        final_states = {str(error_node)}
+        # final_states = {error_node}
+    )
     
+    return dfa
 
 """製作 DFA"""
 def build_dfa(G, path, mapping, initial_node, error_node, unsat_condition):
     states = set()
     input_symbols = set()
     unsat_node = []
-
+    
     # 製作 input_symbols、states
     total_edges = G.edges()
     for start, end in total_edges:
@@ -116,15 +166,16 @@ def build_dfa(G, path, mapping, initial_node, error_node, unsat_condition):
                 transitions[str(path[i])][symbol] = str(end) 
             
     # 將 unsat node 連至 reject_node
-    for node in unsat_node:
-        if node not in path:
-            transitions[str(node)][mapping.get(unsat_condition)] = 'reject_node'
-            # 若 unsat_node 不在 path 裡，更改走進 unsat node 的邊
-            for edge in G.edges():
-                if edge[0] != edge[1]:
-                    label = get_symbol(G, edge[0], edge[1])
-                    if edge[1] == node:
-                        transitions[str(edge[0])][label] = str(node)
+    if unsat_condition != 0:
+        for node in unsat_node:
+            if node not in path:
+                transitions[str(node)][mapping.get(unsat_condition)] = 'reject_node'
+                # 若 unsat_node 不在 path 裡，更改走進 unsat node 的邊
+                for edge in G.edges():
+                    if edge[0] != edge[1]:
+                        label = get_symbol(G, edge[0], edge[1])
+                        if edge[1] == node:
+                            transitions[str(edge[0])][label] = str(node)
 
     dfa = DFA(
         states = states,
